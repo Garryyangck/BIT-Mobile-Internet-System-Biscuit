c43ef2ba1f82b7dd859a5d77548ebc9734f6e42e
Garry <yanggary0127@outlook.com>
2024-12-05 23:11:04 +0800

    business模块增加TestController

---
+++ business/src/main/java/com/garry/biscuit/business/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.garry.biscuit.business.controller;
+
+import com.garry.biscuit.common.vo.ResponseVo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope // 允许随Nacos动态更新
+public class TestController {
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public ResponseVo<String> hello() {
+        return ResponseVo.success("hello business! " + testNacos);
+    }
+}
